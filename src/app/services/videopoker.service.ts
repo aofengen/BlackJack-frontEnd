46297174f428373ea8d0df6783fc51166ff6398a
Aaron Ofengender <aofengen@gmail.com>
2018-05-25 12:18:01 -0400

    video poker gameplay fully functional. Next step: save stats to DB and setup leaderboards

--- src/app/services/videopoker.service.ts
+++ src/app/services/videopoker.service.ts
@@ -25,6 +25,154 @@ export class VideopokerService {
         return x;
     }
 
+    dealCards(p, d) {
+        for (let i = 0; i < 5; i++) {
+            p.hand.push(d[0]);
+            d.shift();
+        }
+    }
+
+    tradeCards(a, p, d) {
+        let i = 5;
+        while(i > 0) {
+            if (a.includes(i)) {
+                p.hand.splice(i-1, 1, d[0]);
+                d.shift();
+            }
+            i--;
+        }
+    }
+
+    checkWinner(hand) {
+		let suits = []
+		let values = []
+				
+		for (let aCard of hand) {
+			suits.push(aCard.suit);
+			values.push(aCard.value);
+		}
+		
+		let cards = [];
+		cards = this.getValues(values);
+		
+		let flush = this.checkFlush(suits);
+		let straight = this.checkStraight(cards);
+		let pair = this.checkPair(values);
+		
+		console.log("flush: " + flush + ", straight: " + straight + ", pair: " + pair);
+		
+		if (flush == true) {
+			if (straight == true) {
+				if (cards.includes(14)) {
+					return "Royal Flush";
+				} else {
+					return "Straight Flush";
+				}
+			} else {
+				return "Flush";
+			}
+		} else if (straight == true) {
+			return "Straight";
+		} else if (pair != "no pair") {
+			return pair;
+		} else {
+			return "nothing...";
+		}
+    }
+    
+    getValues(values) {
+		let num = 0;
+		let numbers = [];
+		for (let value of values) {
+			switch(value) {
+				case "TWO": num=2; break;
+				case "THREE": num=3; break;
+				case "FOUR": num=4; break;
+				case "FIVE": num=5; break;
+				case "SIX": num=6; break;
+				case "SEVEN": num=7; break;
+				case "EIGHT": num=8; break;
+				case "NINE": num=9; break;
+				case "TEN": num=10; break;
+				case "JACK": num=11; break;
+				case "QUEEN": num=12; break;
+				case "KING": num=13; break;
+				case "ACE": num=1; break;
+				default: break;
+			}
+			
+			let x = numbers.length;
+			if (x == 0) {
+				numbers.push(num);
+			} else {
+				for (let i = 0; i <= x; i++) {
+					if (i == x) {
+						numbers.push(num);
+					} else if (num <= numbers[i]) {
+						numbers.splice(i, 0, num);
+						break;
+					}
+				}
+			}
+		}
+		
+		while (numbers.includes(1)) {
+			console.log("testing ace");
+			if (numbers.includes(2) && numbers.includes(3) && numbers.includes(4) && numbers.includes(5)) {
+				break;
+			} else {
+				let index = numbers.indexOf(1);
+				numbers.splice(numbers.indexOf(1), 1).push(14);
+			}
+		}
+ 		
+		return numbers;
+	}
+
+	checkPair(values) {
+		let isPair = "";
+		let matches = 0;
+		for (let i = 0; i < values.length; i++) {
+			for (let j = i + 1; j < values.length; j++) {
+				if (values[i] == values[j]) {
+					matches++;
+				}
+			}
+		}
+		
+		switch (matches) {
+			case 0: isPair = "no pair"; break;
+			case 1: isPair = "One Pair"; break;
+			case 2: isPair = "Two Pair"; break;
+			case 3: isPair = "Three of a Kind"; break;
+			case 4: isPair = "Full House"; break;
+			case 6: isPair = "Four of a Kind"; break;
+			default: isPair = "no pair"; break;
+		}
+		
+		return isPair;
+	}
+
+	checkStraight(cards) {
+		let i = 0;
+		while(i < 4) {
+			if ((cards[i] + 1) != cards[i+1]) {
+				return false;
+			}
+			i++;
+		}
+		return true;
+	}
+
+	checkFlush(suits) {
+		let suit = suits[0];
+		if (suit == suits[1] && suit == suits[2] && suit == suits[3] && suit == suits[4]) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
     postStats() {
         let userData = {
             // handsWon: Number(localStorage.getItem("handsWon")),
--- src/app/videopoker/videopoker.component.ts
+++ src/app/videopoker/videopoker.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { VideopokerService } from '../services/videopoker.service';
 
 @Component({
@@ -10,11 +11,115 @@ export class VideopokerComponent implements OnInit {
 
   constructor(private vs: VideopokerService) { }
 
+  playerMoney: number;
+  playerMoneyAvailable: number;
+  playerBet: number;
+  
+  tradingCards = false;
+  isPlayerBet = false;
+  handOver = false;
+  outOfMoney = false;
+
+  deck = {
+    hand: [],
+    result: ""
+  }
+
+  mainDeck = [];
+  tradeCards = [];
+
   ngOnInit() {
-    this.vs.getMainDeck();
+    this.mainDeck = this.vs.getMainDeck();
+    if (Number(localStorage.getItem("money")) > 0) {
+      this.playerMoney = Number(localStorage.getItem("money"));
+    } else {
+      this.playerMoney = 50;
+    }
   }
 
   postStats() {
     this.vs.postStats();
+  }
+
+  getBet(form: NgForm) {
+    this.playerMoneyAvailable = this.playerMoney;
+    this.playerBet = Number(form.value.amount);
+    if (isNaN(this.playerBet)) {
+      alert("Please enter a number.");
+    } else if (this.playerBet < 1) {
+      alert("You must bet at least $1!");
+    } else if (this.playerBet > 5) {
+      alert("You have exceeded the maximum bet allowed!");
+    } else if (this.playerBet > this.playerMoney) {
+      alert("You can't bet more than you have!");
+    } else {
+      this.playerMoneyAvailable -= this.playerBet;
+      this.isPlayerBet = true;
+
+      this.vs.dealCards(this.deck, this.mainDeck);
+    }
+  }
+
+  reset() {
+    this.mainDeck = this.vs.getMainDeck();
+    this.deck.hand = [];
+    this.deck.result = "";
+    this.handOver = false;
+    this.tradingCards = false;
+    this.vs.dealCards(this.deck, this.mainDeck);
+  }
+
+  tradeCard(x) {
+    if (this.tradeCards.includes(x)) return;
+    else this.tradeCards.push(x); 
+  }
+
+  keepCard(x) {
+    if (this.tradeCards.includes(x)) this.tradeCards.splice(this.tradeCards.indexOf(x), 1);
+    else return;
+  }
+
+  throwCards() {
+    this.tradingCards = true;
+    this.vs.tradeCards(this.tradeCards, this.deck, this.mainDeck);
+    this.deck.result = this.vs.checkWinner(this.deck.hand);
+    console.log(this.deck.result);
+    this.playerMoney = this.handleMoney(this.deck.result, this.playerMoney, this.playerBet);
+    this.handOver = true;
+  }
+
+  handleMoney(result, pMoney, pBet) {
+    switch(result) {
+			case "Royal Flush":
+				pMoney += pBet * 250;
+				break;
+			case "Straight Flush": 
+				pMoney += pBet * 100;
+				break;
+			case "Four of a Kind":
+				pMoney += pBet * 40;
+				break;
+			case "Full House":
+				pMoney += pBet * 15;
+				break;
+			case "Flush":
+				pMoney += pBet * 8;
+				break;
+			case "Straight":
+				pMoney += pBet * 5;
+				break;
+			case "Three of a Kind":
+				pMoney += pBet * 3;
+				break;
+			case "Two Pair":
+				pMoney += pBet;
+        break;
+      case "nothing...":
+        pMoney -= pBet;
+        break;
+			default:
+				break;
+		}
+		return pMoney;
   }
 }
